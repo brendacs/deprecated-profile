d66b7da95e42d75d03e9352a5cdf1374d162d756
brendacs <brenda.zhang2@gmail.com>
2020-03-20 21:47:05 -0700

    Separate subscribe form as component

---
+++ src/components/SubscribeForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="mc_embed_signup">
+    <form action="https://dev.us19.list-manage.com/subscribe/post?u=245ff7f1674733c41a66fc34a&amp;id=a071fd49f6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+        <div class="mc-field-group">
+          <div>
+            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="name@email.com"/>
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="primary-btn">
+          </div>
+        </div>
+        <div id="mce-responses">
+          <!-- <p class="response" id="mce-error-response" style="display:none"></p> -->
+          <p class="response" id="mce-success-response" style="display:none"></p>
+        </div>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_245ff7f1674733c41a66fc34a_a071fd49f6" tabindex="-1" value=""></div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubscribeForm'
+}
+</script>
+
+<style lang="scss" scoped>
+form {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 20px 0;
+}
+
+#mc_embed_signup_scroll {
+	display: flex;
+	/* places error message under inputs */
+	flex-direction: column;
+}
+
+.mc-field-group div {
+	display: flex;
+	margin-bottom: 10px;
+}
+
+input[type=email] {
+	width: 25vw;
+	height: 50px;
+	padding: 0 20px;
+	color: rgb(0, 211, 134);
+	border: 1px solid #d7d7d7;
+
+	@media screen and (max-width: 768px) {
+		width: 50vw;
+	}
+}
+
+label {
+	text-align: left;
+}
+
+#mc_embed_signup input.mce_inline_error {
+		border-color: #cd0000;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,24 +4,7 @@
 		<p>
 			If you like what I write in words or in code, subscribe to my monthly newsletter! Unsubscribe with a click from your inbox anytime.
 		</p>
-		<div id="mc_embed_signup">
-			<form action="https://dev.us19.list-manage.com/subscribe/post?u=245ff7f1674733c41a66fc34a&amp;id=a071fd49f6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<div id="mc_embed_signup_scroll">
-					<div class="mc-field-group">
-						<div>
-							<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="name@email.com"/>
-							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="primary-btn">
-						</div>
-					</div>
-					<div id="mce-responses">
-						<!-- <p class="response" id="mce-error-response" style="display:none"></p> -->
-						<p class="response" id="mce-success-response" style="display:none"></p>
-					</div>
-					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_245ff7f1674733c41a66fc34a_a071fd49f6" tabindex="-1" value=""></div>
-				</div>
-			</form>
-		</div>
+		<SubscribeForm />
 		<h2>Virtual Presence</h2>
 		<p>
 			Other than through my blog and reflections, there are a few other ways to stay up to speed with what I'm working on or chat with me.
@@ -49,8 +32,11 @@
 </template>
 
 <script>
+import SubscribeForm from '../components/SubscribeForm.vue'
+
 export default {
 	name: 'Contact',
+	components: SubscribeForm,
 	data() {
 		return {
 			copy: () => {
@@ -85,43 +71,5 @@ export default {
 		display: block;
 		font-size: 12px;
 	}
-}
-
-form {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	padding: 20px 0;
-}
-
-#mc_embed_signup_scroll {
-	display: flex;
-	/* places error message under inputs */
-	flex-direction: column;
-}
-
-.mc-field-group div {
-	display: flex;
-	margin-bottom: 10px;
-}
-
-input[type=email] {
-	width: 25vw;
-	height: 50px;
-	padding: 0 20px;
-	color: rgb(0, 211, 134);
-	border: 1px solid #d7d7d7;
-
-	@media screen and (max-width: 768px) {
-		width: 50vw;
-	}
-}
-
-label {
-	text-align: left;
-}
-
-#mc_embed_signup input.mce_inline_error {
-		border-color: #cd0000;
 }
 </style>
