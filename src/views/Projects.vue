bb2f8e941655a45e589001161b8a6cc753507fe4
brendacs <brenda.zhang2@gmail.com>
2020-04-23 07:08:08 -0700

    Open links in new tab

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -8,34 +8,34 @@
     <h2>Finished</h2>
     <p>Projects that are completely finished and are actively being maintained.</p>
     <p>
-      <a href="https://github.com/brendacs/me">brendacs.dev</a>. Does this website count? Though using Vue for this simple website is a bit overkill and a static site or blog generator like Jekyll might have been a more sensible choice, I wanted to ease into the framework and stop procastinating on using it.
+      <a href="https://github.com/brendacs/me" target="_blank">brendacs.dev</a>. Does this website count? Though using Vue for this simple website is a bit overkill and a static site or blog generator like Jekyll might have been a more sensible choice, I wanted to ease into the framework and stop procastinating on using it.
     </p>
     <p>
-      <a href="https://github.com/brendacs/stop-bot">Discord Stop Bot</a>. A message moderation bot that automatically "stops" and deletes messages for Discord server admins and mods.
+      <a href="https://github.com/brendacs/stop-bot" target="_blank">Discord Stop Bot</a>. A message moderation bot that automatically "stops" and deletes messages for Discord server admins and mods.
     </p>
     <p>
-      <a href="https://github.com/brendacs/desktop">Desktop Personal Portfolio</a>. The desktop is the window to a developer's soul. This is a personal portfolio I modeled after my own Mac desktop.
+      <a href="https://github.com/brendacs/desktop" target="_blank">Desktop Personal Portfolio</a>. The desktop is the window to a developer's soul. This is a personal portfolio I modeled after my own Mac desktop.
     </p>
     <p>
-      <a href="https://github.com/brendacs/csch">CS Career Hackers</a>. A revamped website built from scratch for the CS Career Hackers nonprofit, which connects a community of software engineers on a mission of lifelong-learning with peers, mentors, and mock interviewers.
+      <a href="https://github.com/brendacs/csch" target="_blank">CS Career Hackers</a>. A revamped website built from scratch for the CS Career Hackers nonprofit, which connects a community of software engineers on a mission of lifelong-learning with peers, mentors, and mock interviewers.
     </p>
 
     <h2>Archived</h2>
     <p>Projects that are completely finished, but no longer being maintained or fixed up.</p>
     <p>
-      <a href="https://github.com/brendacs/rolley-py">Rolley</a>. A Python-based role management bot for Discord, where members of a server use emoji reacts to self-assign roles.
+      <a href="https://github.com/brendacs/rolley-py" target="_blank">Rolley</a>. A Python-based role management bot for Discord, where members of a server use emoji reacts to self-assign roles.
     </p>
     <p>
-      <a href="https://github.com/brendacs/referobot">Referobot</a>. A referral bot, created for referral-based Discord servers to easily count invites per member, top referrers, and number of members in the server.
+      <a href="https://github.com/brendacs/referobot" target="_blank">Referobot</a>. A referral bot, created for referral-based Discord servers to easily count invites per member, top referrers, and number of members in the server.
     </p>
     <p>
-      <a href="https://github.com/brendacs/devils-advocate">Devil's Advocate</a> (freelance). Weekly newsletter that sends political articles subscribers will disagree with in order to foster more open-minded dialog. Includes <a href="https://github.com/brendacs/devils-advocate-api">backend</a> for automatically categorizing subscribers based on quiz answers and assigning MailChimp tags.
+      <a href="https://github.com/brendacs/devils-advocate" target="_blank">Devil's Advocate</a> (freelance). Weekly newsletter that sends political articles subscribers will disagree with in order to foster more open-minded dialog. Includes <a href="https://github.com/brendacs/devils-advocate-api">backend</a> for automatically categorizing subscribers based on quiz answers and assigning MailChimp tags.
     </p>
     <p>
-      <a href="https://github.com/Registria/Liquefy">Liquefy</a>. Liquid language filtering app for non-technical <a href="https://www.registria.com/">Registria</a> staff. Input any block of code with Liquid templating, and it’ll filter it for the Liquid snippets.
+      <a href="https://github.com/Registria/Liquefy" target="_blank">Liquefy</a>. Liquid language filtering app for non-technical <a href="https://www.registria.com/">Registria</a> staff. Input any block of code with Liquid templating, and it’ll filter it for the Liquid snippets.
     </p>
     <p>
-      <a href="https://github.com/brendacs/newsworthy-tab">Newsworthy New Tab</a>. This one is a little embarrassing since I built it when I was first learning how to code, but I tried my hand at building a Chrome extension, a new tab page.
+      <a href="https://github.com/brendacs/newsworthy-tab" target="_blank">Newsworthy New Tab</a>. This one is a little embarrassing since I built it when I was first learning how to code, but I tried my hand at building a Chrome extension, a new tab page.
     </p>
 
     <h2>New</h2>
@@ -43,7 +43,7 @@
       New projects that I am actively building and are in their very early stages.
     </p>
     <p>
-      <a href="https://colludia.com">Colludia Gaming Blog</a>. A game blog prioritizing niche indie games, while also shining light on the indie developers behind them all.
+      <a href="https://colludia.com" target="_blank">Colludia Gaming Blog</a>. A game blog prioritizing niche indie games, while also shining light on the indie developers behind them all.
     </p>
     <p>
       <a>Spoonfed</a>. More info on this soon. Currently, a bit of a secret.
@@ -55,10 +55,10 @@
     <h2>Revisiting</h2>
     <p>Sometimes I find old files and think, "Hm, I like the concept of this enough to fix it up."</p>
     <p>
-      <a href="https://github.com/brendacs/perks">Perks.Guide Redesign</a>. Building on top of the concept of the original Perks.Guide to add more companies, better filtering, and cleaner UI.
+      <a href="https://github.com/brendacs/perks" target="_blank">Perks.Guide Redesign</a>. Building on top of the concept of the original Perks.Guide to add more companies, better filtering, and cleaner UI.
     </p>
     <p>
-      <a href="https://github.com/brendacs/sleep-outreach-project">Sleep Outreach Project</a>. An interactive website about the benefits of sleep, built for my favorite psychology class in college.
+      <a href="https://github.com/brendacs/sleep-outreach-project" target="_blank">Sleep Outreach Project</a>. An interactive website about the benefits of sleep, built for my favorite psychology class in college.
     </p>
 
     <h2>Unfinished</h2>
@@ -66,7 +66,7 @@
       I was hoping you wouldn't get all the way down here.
     </p>
     <p>
-      <a href="https://github.com/brendacs/52in52/blob/master/log.md">52in52</a>. A challenge I made for myself to do 52 projects in 52 weeks a few years ago when I was first discovering frontend and the power of personal side projects in learning... I only did 4 weeks. Maybe I'll try this again sometime.
+      <a href="https://github.com/brendacs/52in52/blob/master/log.md" target="_blank">52in52</a>. A challenge I made for myself to do 52 projects in 52 weeks a few years ago when I was first discovering frontend and the power of personal side projects in learning... I only did 4 weeks. Maybe I'll try this again sometime.
     </p>
   </div>
 </template>
